6d40893915395676dffb761aaa47fa3939d4471a
Anastacio <gl0inn@hotmail.com>
2022-07-07 15:21:24 -0300

    Added products and new index page

--- auctions/templatetags/auction_extras.py
+++ auctions/templatetags/auction_extras.py
@@ -1,5 +1,6 @@
 from django import template
 import os
+from auctions.models import *
 
 register = template.Library()
 
@@ -9,3 +10,10 @@ def get_image(prod):
     Returns the file name.
     """
     return os.path.basename(prod.image.name)
+
+@register.filter
+def get_bid(prod):
+    """
+    Return the last bid of a product
+    """
+    return Bid.objects.filter(product = prod.id).last()
